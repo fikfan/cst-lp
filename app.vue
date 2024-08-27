9bf7382492512cdd0e94ea65106bfb6320556878
Fikri <fikfan@gmail.com>
2024-08-28 03:09:53 +0800

    added favicon and fixed title

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+  title: 'CST SDN. BHD. -- TESTMODE',
+  meta: [
+    { name: 'description', content: "CST SDN. BHD. REDESIGN TEST MODE" }
+  ],
+})
+
+</script>
